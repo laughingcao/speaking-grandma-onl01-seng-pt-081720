94d3f0795d171df2c57724342c57d3874ae7a217
William T Cao <williamacao@gmail.com>
2020-08-21 04:37:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif #{phrase} != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY"
+    return 
   else
-    return "NO, NOT SINCE 1938!"
+    return 
   end
 end
