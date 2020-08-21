ba5e69e2f8f219c2b3a89b68b399e555907eea00
William T Cao <williamacao@gmail.com>
2020-08-21 04:26:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     'I LOVE YOU TOO PUMPKIN!'
   end
 end
